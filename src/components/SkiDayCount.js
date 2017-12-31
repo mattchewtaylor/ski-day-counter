4151cfc87c4684c939f1ded2f28b6d37b524e380
Matt Taylor <matthewbehantaylor@gmail.com>
2017-12-30 19:22:44 -0500

    destructuring components + adding icons

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,20 +1,37 @@
-import React from 'react'
 import '../stylesheets/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 
-export const SkiDayCount = React.createClass({
-	render() {
-		return (
+const percentToDecimal = (decimal) => {
+	return ((decimal * 100) + "%")
+}
+
+const calcGoalProgress = (total, goal) => {
+	return percentToDecimal(total/goal)
+}
+
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
 			<div className="ski-day-count">
 				<div className="total-days">
-					<span>5 days</span>
+					<span>{total}</span>
+						<Calendar />
+					<span>days</span>
 				</div>
 				<div className="powder-days">
-					<span>2 days</span>
+					<span>{powder}</span>
+						<SnowFlake />
+					<span>days</span>
 				</div>
 				<div className="backcountry-days">
-					<span>1 hiking day</span>
+					<span>{backcountry}</span>
+						<Terrain />
+					<span>days</span>
+				</div>
+				<div>
+					<span>
+						{calcGoalProgress(total, goal)}
+					</span>
 				</div>
 			</div>
-		)
-	}
-})
+)
