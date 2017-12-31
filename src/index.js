3603e5939e7a9711c45d26ef95dc158cce485d7f
Matt Taylor <matthewbehantaylor@gmail.com>
2017-12-30 19:43:44 -0500

    counter list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 render(
-	<SkiDayCount total={50}
-				 powder={20}
-				 backcountry={10}
-				 goal={100}/>,
+	<SkiDayList days ={
+		[
+			{
+				resort: "Aspen",
+				date: new Date("12/30/2017"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort: "Vail",
+				date: new Date("1/30/2018"),
+				powder: false,
+				backcountry: true
+			},
+			{
+				resort: "A-Basin",
+				date: new Date("2/30/2018"),
+				powder: true,
+				backcountry: false
+			}
+		]
+	}/>,
 	document.getElementById('react-container')
 )
